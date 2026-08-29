6fd000716b4d1547178652ea42a61dbf69bdbf69
Harsh Joshi <joshi407harsh@gmail.com>
2026-08-29 15:38:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -125,6 +125,7 @@ const defaultDevOrigins = [
 const allowedOrigins = [
   ...(process.env.NODE_ENV === 'production' ? [] : defaultDevOrigins),
   ...envOrigins,
+  'https://havenhomespunjab.in',
 ];
 
 const uniqueAllowedOrigins = [...new Set(allowedOrigins)];
